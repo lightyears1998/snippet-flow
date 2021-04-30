1765cba27093ad020e30c72d6668fdf18e0bec6a
lightyears1998 <lightyears1998@hotmail.com>
2021-04-30 21:03:23 +0800

    oh: frontend stuff is hard

--- frontend/src/pages/post/editor.tsx
+++ frontend/src/pages/post/editor.tsx
@@ -1,5 +1,22 @@
+import {
+  Grid, Button, Container, TextField
+} from "@material-ui/core";
+import React from "react";
+
 export const PostEditorPage = () => {
-  return <></>;
+  return (<>
+    <Container>
+      <h2>Post Editor</h2>
+      <form noValidate autoComplete="off">
+        <TextField id="post-title" label="Post Title" variant="outlined" fullWidth />
+      </form>
+      <Grid container justify="center" spacing={2}>
+        <Grid item xs={3}><Button fullWidth variant="outlined">引用 Post</Button></Grid>
+        <Grid item xs={3}><Button fullWidth variant="outlined">添加 Snippet</Button></Grid>
+      </Grid>
+    </Container>
+  </>
+  );
 };
 
 export default PostEditorPage;
